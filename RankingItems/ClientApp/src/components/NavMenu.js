49881904588321b908f39191b293c92588453df9
5ecurealf <alfielloyd12@gmail.com>
2023-11-26 16:27:51 +0000

    Provide link for the user to navigate to the rank items page

--- RankingItems/ClientApp/src/components/NavMenu.js
+++ RankingItems/ClientApp/src/components/NavMenu.js
@@ -35,9 +35,12 @@ export class NavMenu extends Component {
               <NavItem>
                 <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
               </NavItem>
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
-              </NavItem>
+                <NavItem>
+                    <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink tag={Link} className="text-dark" to="/rank-items">Rank Items</NavLink>
+                </NavItem>
             </ul>
           </Collapse>
         </Navbar>
