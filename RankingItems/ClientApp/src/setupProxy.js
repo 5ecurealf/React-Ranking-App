22f596a04e7441dee14a1eb1f9b9c0184c1f5d48
5ecurealf <alfielloyd12@gmail.com>
2023-11-26 18:36:28 +0000

    Use ASP template to connect React front end to the .NET backend

--- RankingItems/ClientApp/src/setupProxy.js
+++ RankingItems/ClientApp/src/setupProxy.js
@@ -5,7 +5,9 @@ const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_H
   env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:3282';
 
 const context = [
-  "/weatherforecast",
+    "/weatherforecast",
+    "/Item",
+
 ];
 
 const onError = (err, req, resp, target) => {
