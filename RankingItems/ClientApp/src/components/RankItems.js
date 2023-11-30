a1681a94d5ccd719c4b219cbda554760704369a9
5ecurealf <alfielloyd12@gmail.com>
2023-11-30 19:03:20 +0000

    Styling the reload button

--- RankingItems/ClientApp/src/components/RankItems.js
+++ RankingItems/ClientApp/src/components/RankItems.js
@@ -85,7 +85,7 @@ const RankItems = ({ items, setItems, dataType, imgArr, localStorageKey }) => {
             <main>
                 <RankingGrid items={items} imgArr={imgArr} drag={drag} allowDrop={allowDrop} drop={drop } />
                 <ItemCollection items={items} drag={drag} imgArr={imgArr} />
-                <button onClick={Reload} style={{"marginTop" : "10px"} }>Reload</button>
+                <button class="reload" role="button" onClick={Reload} > Reload </button >
             </main>
             :<div>...Loading</div>
    )
